079d62e156d0bd78ed505afa8705843d14f9b5f3
William Lennartsson <william.j.lennartsson@gmail.com>
2019-08-30 08:57:36 +0200

    Uppgifter för arrayer

---
+++ uppg-arrayer.java
@@ -0,0 +1,27 @@
+// Arrayer
+
+// Skriv en metod som tar in och sorterar en int[]
+// Public static void sortIntArr(int[] list){}
+// ex. {3, 5, 1, 2} -> {1, 2, 3, 5}
+
+// Gör så att förgående metod returnerar den sorterade arrayen
+
+// Lägg till en parameter (bool reversed) som bestämmer om den skall sortera bakvänt 
+// ex. {3, 5, 1, 2} -> {5, 3, 2, 1}
+
+// Skriv ett program som med hjälp av en tvådimensionell array skriver ut följande: 
+
+// - - - - -
+// - - - - -
+// - - - - -
+// - - - - -
+// - - - - -
+
+// Lägg till en check så att följande skrivs ut 
+
+// 0  -  -  -  - 
+// 0  -  -  -  - 
+// 0  -  -  -  - 
+// 0  -  -  -  - 
+// 0  0  0  0  0
+
