b36410f92a6e45f7a55b727ecfd2be0660aac4f4
William Lennartsson <william.j.lennartsson@gmail.com>
2019-09-13 11:02:24 +0200

    Arv

---
+++ arv/Student.java
@@ -0,0 +1,10 @@
+
+public class Student extends Person{
+    private int yhPoäng;
+
+    public Student(String name, int age, int yhPoäng) {
+        super(name, age);
+        this.yhPoäng = yhPoäng;
+    }
+
+}
---
+++ arv/Teacher.java
@@ -0,0 +1,11 @@
+
+public class Teacher extends Person {
+
+    private int whiteBoardSkills = 100;
+    private String typeOfCoffeeCup;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+}
