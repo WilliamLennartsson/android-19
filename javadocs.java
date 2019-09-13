4e4626bf017782784d55c28b55ac05aad61a7e18
William Lennartsson <william.j.lennartsson@gmail.com>
2019-09-13 11:03:42 +0200

    Class with docs gen comments

---
+++ javadocs.java
@@ -0,0 +1,23 @@
+// Class with java docs comments
+
+/**
+ * Detta är en båtklass ute på havet.
+ * @author Willy L
+ */
+public class Boat {
+    private String name;
+    /**
+     * @param name Name of the boat
+     * @param size Size of the boat
+     */
+    public Boat(String name, int size) {
+
+    }
+
+    /**
+     * @return Returns the name of the boat
+     */
+    public String getName() {
+        return name;
+    }
+}
