a691a70b5ef3308bfec27e74316b73ee89eafae3
William Lennartsson <william.j.lennartsson@gmail.com>
2019-09-22 23:15:38 +0200

    Labb 3 template

---
+++ Labb3-Template/Game.java
@@ -0,0 +1,17 @@
+// Glöm inte paketnamn här.
+
+public class Game {
+    private Player player;
+
+    // En metod för att skapa upp spelaren.
+    public Player playerSetup(){return null;}
+    // En metod för att starta spelet
+    public void start(){}
+    // Själva gameloopen. Här skall logiken ligga som hindrar programmet från att avslutas.
+    public void gameLoop(){}
+    // En metod som hanterar menyalternativ
+    public void handleMenu(){}
+    // En metod för att starta en battle mellan en player och ett monster
+    public void battle(Player player, Monster monster){}
+
+}
